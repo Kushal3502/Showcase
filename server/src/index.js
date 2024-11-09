8fdc05e3b287e1dcee765b88c6577c7ba27554b1
Kushal3502 <kushal2002personal@gmail.com>
2024-11-09 11:15:04 +0530

    db connection

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) =>
+      console.log("MongoDB connection error :: ", error)
+    );
+    app.listen(port, () => console.log("Server is running on port :: ", port));
+  })
+  .catch((error) => console.log("MongoDB connection error :: ", error));
