cd9252bbbc74b96132744d79199bb89d1da985f8
Kushal3502 <kushal2002personal@gmail.com>
2024-11-10 20:26:54 +0530

    post controller

--- server/src/controller/post.controller.js
+++ server/src/controller/post.controller.js
@@ -1,4 +1,6 @@
+import mongoose, { isValidObjectId, mongo } from "mongoose";
 import { Post } from "../models/post.model.js";
+import { User } from "../models/user.model.js";
 
 export const createPost = async (req, res) => {
   const { content, image } = req.body;
@@ -79,10 +81,170 @@ export const getAllPosts = async (req, res) => {
   }
 };
 
-export const getUserPosts = async (req, res) => {};
+export const getUserPosts = async (req, res) => {
+  const { userId } = req.params;
 
-export const getPostById = async (req, res) => {};
+  if (!userId)
+    return res.status(400).json({ success: false, message: "Invalid userId" });
 
-export const updatePost = async (req, res) => {};
+  try {
+    // check if user exists or not
+    const isUserExists = await User.findById(userId);
 
-export const deletePost = async (req, res) => {};
+    if (!isUserExists)
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+
+    // find posts
+    const posts = await Post.aggregate([
+      {
+        $match: {
+          owner: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                username: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $unwind: "$owner",
+      },
+    ]);
+
+    if (!posts || posts.length === 0)
+      return res.status(400).json({ success: false, message: "No post found" });
+
+    return res
+      .status(200)
+      .json({ success: true, posts, message: "Posts fetched successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  const { postId } = req.params;
+
+  if (!postId)
+    return res.status(400).json({ success: false, message: "Invalid postId" });
+
+  try {
+    const post = await Post.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(postId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "owner",
+          foreignField: "_id",
+          as: "owner",
+          pipeline: [
+            {
+              $project: {
+                username: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        $unwind: "$owner",
+      },
+    ]);
+
+    if (!post)
+      return res.status(400).json({ success: false, message: "No post found" });
+
+    return res.status(200).json({
+      success: true,
+      post: post[0],
+      message: "Post fetched successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { postId } = req.params;
+
+  if (!postId)
+    return res.status(400).json({ success: false, message: "Invalid postId" });
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post)
+      return res
+        .status(400)
+        .json({ success: false, message: "Post not found" });
+
+    const { content, image } = req.body;
+    const updatedFields = {};
+
+    if (content !== undefined) updatedFields.content = content;
+    if (image !== undefined) updatedFields.image = image;
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $set: updatedFields,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      post: updatedPost,
+      message: "Post updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { postId } = req.params;
+
+  if (!postId)
+    return res.status(400).json({ success: false, message: "Invalid postId" });
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post)
+      return res
+        .status(400)
+        .json({ success: false, message: "Post not found" });
+
+      await Post.findByIdAndDelete(postId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import { validateAccessToken } from "../middlewares/auth.middleware.js";
-import { createPost, getAllPosts } from "../controller/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  getAllPosts,
+  getPostById,
+  getUserPosts,
+  updatePost,
+} from "../controller/post.controller.js";
 
 const router = Router();
 
 router.route("/").get(getAllPosts).post(validateAccessToken, createPost);
+
+router.route("/user/:userId").get(getUserPosts);
+
+router
+  .route("/:postId")
+  .get(getPostById)
+  .patch(validateAccessToken, updatePost)
+  .delete(validateAccessToken, deletePost);
 
 export default router;
