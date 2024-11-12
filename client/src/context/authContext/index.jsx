ec3530bca8f95d3101f41b6acfe7a291b92078f6
Kushal3502 <kushal2002personal@gmail.com>
2024-11-12 11:39:42 +0530

    auth context

--- client/src/context/authContext/index.jsx
+++ client/src/context/authContext/index.jsx
@@ -1,4 +1,4 @@
-import { get } from "@/utils/api";
+import { get, post } from "@/utils/api";
 import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(null);
@@ -12,12 +12,62 @@ export const AuthContextProvider = ({ children }) => {
     if (response.currentUser) setUser(response.currentUser);
   };
 
+  const refreshToken = async () => {
+    try {
+      const response = await post("users/refresh-access-token");
+
+      if (!response.accessToken) {
+        logout();
+      }
+    } catch (error) {
+      logout();
+    }
+  };
+
+  const register = async (data) => {
+    try {
+      const response = await post("users/register", data);
+
+      if (response) return response.user;
+      else return null;
+    } catch (error) {
+      console.log("Register error :: ", error);
+    }
+  };
+
+  const login = async (data) => {
+    try {
+      const response = await post("users/login", data);
+
+      if (response.success) {
+        setUser(response.user);
+      }
+
+      return response.user;
+    } catch (error) {
+      console.log("Login error :: ", error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await post("users/logout");
+
+      if (response.success) {
+        setUser(null);
+      }
+    } catch (error) {
+      console.log("Logout error :: ", error);
+    }
+  };
+
   useEffect(() => {
     authStatus();
+    refreshToken();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, register, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
